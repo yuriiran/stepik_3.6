813f6c3d051f7cbe781930c44112a408cac32840
John Doe <yurii01031989@gmail.com>
2020-05-04 11:25:08 +0800

    correct amend

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default=None)
+    parser.addoption('--language', action='store', default="ru")
 
 @pytest.fixture(scope="function")
 def browser(request):
--- test_items.py
+++ test_items.py
@@ -4,8 +4,8 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_cart_button(browser):
     browser.get(link)
-    time.sleep(5)
+    #time.sleep(5)
     #проверяем если ли кнопка
-    cart_button = browser.find_element_by_css_selector("#add_to_basket_form")
+    cart_button = browser.find_element_by_class_name("btn.btn-lg.btn-primary.btn-add-to-basket")
     assert cart_button, "should be a cart button"
     
